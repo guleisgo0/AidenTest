4fe98bef2e7ec59248bc17ac877e63d263a0917d
Aiden <04993@farnborough.ac.uk>
2020-11-06 09:57:13 +0000

    i decided we needed more variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ public class Main
     public static void main(String[] args)
     {
         String messages = "gang gang";
+        int longa = 0;
         System.out.println(messages);
-        System.out.println(messages);
+        longa = messages.length();
+        System.out.println(longa);
     }
 }
