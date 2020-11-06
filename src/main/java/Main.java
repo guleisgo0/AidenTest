b835a7cb8568054edba9e959656170105288f087
Student   BAKER-STANLEY Aiden <04993@farnborough.ac.uk>
2020-11-06 09:54:29 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String messages = "gang gang";
+        System.out.println(messages);
+        System.out.println(messages);
+    }
+}
