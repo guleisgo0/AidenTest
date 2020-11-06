054af732f6907e67c83ec6939457235e4f4a2f4f
Aiden <04993@farnborough.ac.uk>
2020-11-06 10:03:08 +0000

    I changed everything bc why not

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,30 @@
-public class Main
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Main
 {
     public static void main(String[] args)
     {
-        String messages = "gang gang";
-        int longa = 0;
-        longa = messages.length();
-        System.out.println(longa);
-        System.out.println("I hate strings");
+        Scanner myObj = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<String>();
+        for (int x=0; x != 6;x++)
+        {
+            System.out.println("Give me a name!");
+            names.add(myObj.nextLine());
+        }
+
+        while (1==1)
+        {
+            System.out.println("Dwarfcheck?");
+            String dwrfCheck = myObj.nextLine();
+            if (names.contains(dwrfCheck))
+            {
+                System.out.println("They're a dwarf!");
+            }
+            else
+            {
+                System.out.println("They're not a dwarf!");
+            }
+        }
     }
 }
